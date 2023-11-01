8c890a16628b0c79a229107e9da100b1ff7a05e4
Antonin Guyon <aguyon@student.42.fr>
2023-11-01 16:21:58 +0100

    correction ft_realloc et implementation de xrealloc

--- src/alloc/ft_realloc.c
+++ src/alloc/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 22:51:35 by aguyon            #+#    #+#             */
-/*   Updated: 2023/10/11 20:16:48 by aguyon           ###   ########.fr       */
+/*   Updated: 2023/11/01 16:21:06 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,24 @@ void	*ft_realloc(void *ptr, size_t original_size, size_t size)
 		return (malloc(size));
 	if (size == 0)
 		return (free(ptr), NULL);
-	if (original_size <= size)
-		return (ptr);
 	new_ptr = malloc(size);
 	if (new_ptr == NULL)
 		return (NULL);
 	ft_memcpy(new_ptr, ptr, original_size);
 	return (free(ptr), new_ptr);
+}
+
+void	*xrealloc(void *ptr, size_t original_size, size_t size)
+{
+	void	*new_ptr;
+
+	if (ptr == NULL)
+		return (xmalloc(size));
+	if (size == 0)
+		return (xfree(ptr), NULL);
+	new_ptr = xmalloc(size);
+	if (new_ptr == NULL)
+		return (NULL);
+	ft_memcpy(new_ptr, ptr, original_size);
+	return (xfree(ptr), new_ptr);
 }
