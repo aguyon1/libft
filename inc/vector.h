75ecf4e88e6080da8aa17812505ecb513ab6e886
Antonin Guyon <aguyon@student.42.fr>
2023-12-07 14:34:16 +0100

    norminette

--- inc/vector.h
+++ inc/vector.h
@@ -6,7 +6,7 @@
 /*   By: aguyon <aguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 14:05:24 by aguyon            #+#    #+#             */
-/*   Updated: 2023/12/07 14:32:56 by aguyon           ###   ########.fr       */
+/*   Updated: 2023/12/07 14:34:07 by aguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,15 @@ typedef struct s_vector
 	size_t	total;
 }	t_vector;
 
-t_vector	*vector_new(size_t capacity , size_t content_size);
+t_vector	*vector_new(size_t capacity, size_t content_size);
 int			vector_init(t_vector *vec, size_t capacity, size_t content_size);
 int			vector_resize(t_vector *vec, size_t count);
 int			vector_push_back(t_vector *vec, void *const item);
 int			vector_set(t_vector *vec, size_t index, void *const item);
 int			vector_at(t_vector *vec, size_t index, void *ret);
-// int		vector_delone(t_vector *vec, size_t index, void (*del)(void *));
-void		vector_clear(t_vector **vec)
+void		vector_clear(t_vector **vec);
 size_t		vector_size(t_vector *vec);
+
+// int		vector_delone(t_vector *vec, size_t index, void (*del)(void *));
 
 #endif
